b000f878c476cf5ce43cb28a24ab478eb6d631f1
Thanu-kv <60957405+Thanu-kv@users.noreply.github.com>
2020-10-09 12:49:39 +0530

    Create calculator.h

---
+++ inc/calculator.h
@@ -0,0 +1,16 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+int addition(int number1,int number2);
+int subtraction(int number1,int number2);
+int multiplication(int number1,int number2);
+int division(int number1,int number2);
+int modulus(int number1,int number2);
+int power(int number1,int number2);
+int factorial(int number1);
+ 
+#endif
